376abb8e26e3f0c877739616f891ce41aaec5c88
Isaiah peter <peterisaiah4fun@gmail.com>
2022-09-06 21:05:53 +0100

    console start

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" console """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ HBNH console """
+    intro = 'Welcome to the hbnb shell.   Type help or ? to list commands.\n'
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """quit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """quit the program"""
+        return True
+
+    def emptyline(self):
+        """don't run empty line"""
+        return False
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
